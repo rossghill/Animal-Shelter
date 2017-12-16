2e38714790b4f6f1a23acb6392a52fb006903600
Ross Hill <rosshill88@gmail.com>
2017-12-16 15:54:03 +0000

    Save method for owner model

--- models/owner.rb
+++ models/owner.rb
@@ -7,4 +7,13 @@ class Owner
     @name = options["name"]
   end
 
+  def save
+    sql = "INSERT INTO owners (name)
+          VALUES ($1)
+          RETURNING id"
+    values = [@name]
+    result = SqlRunner.run(sql, values)
+    @id = result[0]['id'].to_i      
+  end
+
 end
