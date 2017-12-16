238472bb752fcaebd2a83f1d7449e60f727fbf5d
Ross Hill <rosshill88@gmail.com>
2017-12-16 15:26:56 +0000

    Save method for animal_type model

--- models/animal_type.rb
+++ models/animal_type.rb
@@ -7,4 +7,13 @@ class AnimalType
     @name = options['name']
   end
 
+  def save()
+    sql = "INSERT INTO animal_types(name)
+          VALUES ($1)
+          RETURNING id"
+    values = [@name]
+    result = SqlRunner.run(sql, values)
+    @id = result[0].to_id    
+  end
+
 end
