69865ee333e55aceac0288654c5b8b4760ef5007
Ross Hill <rosshill88@gmail.com>
2017-12-16 12:38:04 +0000

    Initialize class created on Adoption model

--- models/adoption.rb
+++ models/adoption.rb
@@ -1,3 +1,10 @@
 class Adoption
 
+  def initialize(options)
+    @id = options['id'].to_i
+    @adoption_date = options['adoption_date']
+    @owner_id = options ['owner_id'].to_i
+    @animal_id = options ['animal_id'].to_i
+  end
+
 end
