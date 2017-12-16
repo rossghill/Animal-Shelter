c9e33552e49f1543f3b9ace337eb3c268dd1f9c9
Ross Hill <rosshill88@gmail.com>
2017-12-16 12:45:30 +0000

    Initialize class created on animal model

--- models/animal.rb
+++ models/animal.rb
@@ -1,5 +1,12 @@
 class Animal
 
+  def initialize(options)
+    @id = options['id'].to_i
+    @name = options['name']
+    @admission_date = options['admission_date']
+    @adoptable = options['adoptable']
+    @animal_type = options['animal_type']
+    @adoption_status = options['adoption_status']
+  end
 
-
-end  
+end
