4d8fd0a578706f6509b7cb459803b0207384b320
Ross Hill <rosshill88@gmail.com>
2017-12-17 16:36:14 +0000

    Added edit, update, delete routes for Animals_controller

--- controllers/animals_controller.rb
+++ controllers/animals_controller.rb
@@ -2,17 +2,34 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/animal')
 
+#List all
 get '/animals/index' do
   @animals = Animal.all()
   erb(:"animals/index")
 end
 
-# get 'animals/index/:id/edit' do
-#   @animals = Animal.find(params['id'])
-#   erb(:edit)
-# end
-
+#List individual
 get '/animals/:id' do
    @animal = Animal.find(params['id'])
   erb(:"animals/show")
+end
+
+#Edit
+get 'animals/:id/edit' do
+  @animal = Animal.find(params['id'])
+  erb(:"animals/edit")
+end
+
+#Update
+post 'animals/:id'
+  animal = Animal.find(params['id'])
+  animal.save()
+  redirect to ('/:id')
+end
+
+#Delete
+post 'animals/:id' do
+  animal = Animal.find(params['id'])
+  animal.delete()
+  redirect to ('/index')
 end
