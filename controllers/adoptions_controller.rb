19acaa220f16d4e2581edb858b9172d5224e2555
Ross Hill <rosshill88@gmail.com>
2017-12-19 22:34:49 +0000

    Added adoptions index page, link to it on navbar, button linking to new adoption

--- controllers/adoptions_controller.rb
+++ controllers/adoptions_controller.rb
@@ -1,2 +1,15 @@
 require('sinatra')
 require('sinatra/contrib/all')
+require_relative('../models/animal')
+require_relative('../models/owner')
+require_relative('../models/adoption')
+
+get '/adoptions/index' do
+  erb(:"adoptions/index")
+end
+
+get '/adoptions/new' do
+  @animals = Animal.all
+  @owners = Owner.all
+  erb(:"adoptions/new")
+end
